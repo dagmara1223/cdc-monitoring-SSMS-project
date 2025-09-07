a104d29ea31745841cf83fc83adff357a0d025b1
Dagmara Krenich <161820137+dagmara1223@users.noreply.github.com>
2025-09-07 17:17:28 +0200

    Create create_tables.sql

---
+++ SQL/create_tables.sql
@@ -0,0 +1,13 @@
+USE CDC_Project; -- the name of your project
+
+CREATE TABLE fromTable(
+	ID INT PRIMARY KEY,
+	Name VARCHAR(50),
+	Age INT
+);
+
+CREATE TABLE toTable(
+	ID INT PRIMARY KEY,
+	Name VARCHAR(50),
+	Age INT
+);
