c919595acfd35aa019b1b1974d2309c425765efe
Dagmara Krenich <161820137+dagmara1223@users.noreply.github.com>
2025-09-07 23:56:22 +0200

    Add files via upload

---
+++ SQL/updating.sql
@@ -0,0 +1,12 @@
+CREATE OR ALTER PROCEDURE Updating
+	@StartID INT, -- from which ID we start
+	@RowCount INT -- how many rows would you like to update 
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	UPDATE fromTable 
+	SET Name = CONCAT('UPDATED_User_', ID) -- new Name will look like : UPDATED_USER_1
+	WHERE ID BETWEEN @StartID AND (@StartID + @RowCount - 1); -- our update range - from start to end
+
+END;
