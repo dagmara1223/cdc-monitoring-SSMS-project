631bdf7892742f9cff11140cd6188b635d20dcd0
Dagmara Krenich <161820137+dagmara1223@users.noreply.github.com>
2025-09-07 17:18:35 +0200

    Add files via upload

---
+++ SQL/create-table.sql
@@ -0,0 +1,14 @@
+USE CDC_Project; -- the name of your project
+
+CREATE TABLE fromTable(
+	ID INT PRIMARY KEY,
+	Name VARCHAR(50),
+	Age INT
+);
+
+CREATE TABLE toTable(
+	ID INT PRIMARY KEY,
+	Name VARCHAR(50),
+	Age INT
+);
+
