a8e3857676a884beef38d0eae6a5068487d110a9
Dagmara Krenich <161820137+dagmara1223@users.noreply.github.com>
2025-10-07 19:32:04 +0200

    Update check-up.sql

--- SQL/check-up.sql
+++ SQL/check-up.sql
@@ -6,29 +6,17 @@ AS
 BEGIN
 	-- Checking whether the database & tables exist
 	IF EXISTS(SELECT * FROM master.sys.databases WHERE name= @DatabaseName)
-	BEGIN
 		PRINT 'Database exists!';
-	END
 	ELSE
-	BEGIN
 		PRINT 'Database doesn''t exists!'
-	END
 
 	IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = @SourceTableName)
-	BEGIN
 		PRINT 'Source table exists!';
-	END
 	ELSE
-	BEGIN
 		PRINT 'Source table doesn''t exist!';
-	END
 
 	IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = @DestinationTableName)
-	BEGIN
 		PRINT 'Destination table exists!';
-	END
 	ELSE
-	BEGIN
 		PRINT 'Destination table doesn''t exist!';
-	END
 END;
